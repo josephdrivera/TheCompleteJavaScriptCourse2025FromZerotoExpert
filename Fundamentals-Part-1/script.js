a89adefeec7109fce995cb28f31639cb8e8a775d
Joseph Rivera <josephdrivera15@gmail.com>
2025-03-19 11:27:26 -0400

    add detailed comments and examples for variables, data types, and operators in script.js for clarity

--- Fundamentals-Part-1/script.js
+++ Fundamentals-Part-1/script.js
@@ -1,3 +1,5 @@
+
+/*
 let js = "Hello, World!";
 console.log(40 + 2 + 21 - 10);
 
@@ -6,5 +8,108 @@ console.log(23);
 console.log("23" + 23);
 
 let firstName = "Jonas"; 
-console.log(firstName); 
+console.log(firstName); // Variable declaration
+console.log(firstName);
+console.log(firstName);
 
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+// Not allowed
+let person = "jonas";
+let PI = 3.1415;
+
+// Reserved keywords
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+// Data types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+//console.log(typeof "Joe");
+//console.log(typeof undefined);
+//console.log(typeof null);
+
+// Type conversion
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+// Type coercion
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2025;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof 23.5);
+console.log(typeof Number(23.5));
+console.log(typeof String(23.5));
+console.log(typeof "23.5");
+console.log(typeof "23.5" + 23);
+console.log(typeof ("23.5" + 23));
+
+
+// Variables
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+var job = 'programmer';
+job = 'teacher'
+
+let lastName = 'Rivera';
+console.log(lastName);
+lastName = 'Smith';
+console.log(lastName);
+
+
+
+// Basic Operators
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10
+x *= 4; // x = x * 4
+x++; // x = x + 1
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(isFullAge);
+*/
+
+// Operator Precedence
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 - 5);
+
+console.log(25 - (10 - 5));
+
+console.log(3 * 5 - 10);
+
+console.log(3 * (5 - 10));
